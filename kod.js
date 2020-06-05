f604a876a58c8322477998ba75e8987bfa20059d
dagensord <66313259+dagensord@users.noreply.github.com>
2020-06-05 18:19:36 +0200

    Uppdaterade koden
    Fixade felbuggen och gav stöd till att köra igenom listan igen

--- kod.js
+++ kod.js
@@ -11,42 +11,47 @@ function laddaOrd() {
       
    }
    xhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-         var nu = new Date().getTime();
-         var startDag = new Date("3 jun 2020").getTime();
-         var index = (Math.floor((nu / 1000 / 60 / 60 / 24) - (startDag / 1000 / 60 / 60 / 24)));
-         ordlista = JSON.parse(xhttp.responseText).ord;
-         ord = ordlista[index].namn.replace(/<script/i, "output");
-         betydelse = ordlista[index].betydelse.replace(/<script/i, "output");
-         exempelmening = ordlista[index].exempelmening.replace(/<script/i, "output");
-         etymologi = ordlista[index].etymologi.replace(/<script/i, "output");
+      if(this.readyState == 4) {
+		 if(this.status == 200) {
+			var nu = new Date().getTime();
+			var startDag = new Date("3 jun 2020").getTime();
+			var index = (Math.floor((nu / 1000 / 60 / 60 / 24) - (startDag / 1000 / 60 / 60 / 24)));
+			ordlista = JSON.parse(xhttp.responseText).ord;
+			while(index >= ordlista.length) {
+				index -= ordlista.length;
+				console.log(index);
+			}
+			ord = ordlista[index].namn.replace(/<script>/i, "VARNING");
+			betydelse = ordlista[index].betydelse.replace(/<script>/i, "VARNING");
+			exempelmening = ordlista[index].exempelmening.replace(/<script>/i, "VARNING");
+			etymologi = ordlista[index].etymologi.replace(/<script>/i, "VARNING");
          
-         innehall("ord", ord.charAt(0).toUpperCase() + ord.substring(1));
-         innehall("rubrikbet", "Vad betyder <output class='kursiv'>" + ord + "</output>?");
-         innehall("rubrikanv", "Hur skulle man kunna använda ordet <output class='kursiv'>" + ord + "</output>?");
-         innehall("rubrikety", "Varifrån kommer ordet <output class='kursiv'>" + ord + "</output>?");
+			innehall("ord", ord.charAt(0).toUpperCase() + ord.substring(1));
+			innehall("rubrikbet", "Vad betyder <output class='kursiv'>" + ord + "</output>?");
+			innehall("rubrikanv", "Hur skulle man kunna använda ordet <output class='kursiv'>" + ord + "</output>?");
+			innehall("rubrikety", "Varifrån kommer ordet <output class='kursiv'>" + ord + "</output>?");
          
-         innehall("betydelse", betydelse);
-         innehall("anvandning", exempelmening);
-         innehall("etymologi", etymologi);
+			innehall("betydelse", betydelse);
+			innehall("anvandning", exempelmening);
+			innehall("etymologi", etymologi);
          
-         if(ordlista[index].lank !== undefined) {
-            document.getElementById("lank").href = ordlista[index].lank;
-         }
-         else {
-            document.getElementById("lank").href = "https://www.svenska.se/tre/?sok=" + ord + "&pz=1";
-         }
+			if(ordlista[index].lank !== undefined) {
+				document.getElementById("lank").href = ordlista[index].lank;
+			}
+			else {
+				document.getElementById("lank").href = "https://www.svenska.se/tre/?sok=" + ord + "&pz=1";
+			}
+		 }
+         else  {
+			innehall("ord", "Fel");
+			innehall("rubrikbet", "Ajdå!");
+			innehall("betydelse", "Ett fel uppstod när orden skulle läsas in. Försök att läsa in sidan igen. Om felet kvarstår kan du behöva kontakta sidans ägare.");
+			innehall("rubrikety", "");
          
-      }
-      else  {
-         innehall("ord", "Fel");
-         innehall("rubrikbet", "Ajdå!");
-         innehall("betydelse", "Ett fel uppstod när orden skulle läsas in. Försök att läsa in sidan igen. Om felet kvarstår kan du behöva kontakta sidans ägare.");
-         innehall("rubrikety", "");
-         
-         innehall("rubrikanv", "");
-         innehall("anvandning", "");
-         innehall("etymologi", "");
+			innehall("rubrikanv", "");
+			innehall("anvandning", "");
+			innehall("etymologi", "");
+		}
       }
    }
    xhttp.open("GET", "https://raw.githubusercontent.com/dagensord/dagensord.github.io/master/ordlista.json", true);
