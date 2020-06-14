37ba947c25e21090615b178370c30577bac12a74
dagensord <66313259+dagensord@users.noreply.github.com>
2020-06-14 11:50:33 +0200

    Storuppdatering
    Uttalsangivelser för svåruttalade ord, allmänna förbättringar i definitioner, fler ord i listan m.m

--- kod.js
+++ kod.js
@@ -9,51 +9,60 @@ function laddaOrd() {
    let xhttp = new XMLHttpRequest();
    xhttp.onerror = function() {
       
-   }
+   };
    xhttp.onreadystatechange = function() {
       if(this.readyState == 4) {
-		 if(this.status == 200) {
-			var nu = new Date().getTime();
-			var startDag = new Date("3 jun 2020").getTime();
-			var index = (Math.floor((nu / 1000 / 60 / 60 / 24) - (startDag / 1000 / 60 / 60 / 24)));
-			ordlista = JSON.parse(xhttp.responseText).ord;
-			while(index >= ordlista.length) {
-				index -= ordlista.length;
-				console.log(index);
-			}
-			ord = ordlista[index].namn.replace(/<script>/i, "VARNING");
-			betydelse = ordlista[index].betydelse.replace(/<script>/i, "VARNING");
-			exempelmening = ordlista[index].exempelmening.replace(/<script>/i, "VARNING");
-			etymologi = ordlista[index].etymologi.replace(/<script>/i, "VARNING");
-         
-			innehall("ord", ord.charAt(0).toUpperCase() + ord.substring(1));
-			innehall("rubrikbet", "Vad betyder <output class='kursiv'>" + ord + "</output>?");
-			innehall("rubrikanv", "Hur skulle man kunna använda ordet <output class='kursiv'>" + ord + "</output>?");
-			innehall("rubrikety", "Varifrån kommer ordet <output class='kursiv'>" + ord + "</output>?");
-         
-			innehall("betydelse", betydelse);
-			innehall("anvandning", exempelmening);
-			innehall("etymologi", etymologi);
-         
-			if(ordlista[index].lank !== undefined) {
-				document.getElementById("lank").href = ordlista[index].lank;
-			}
-			else {
-				document.getElementById("lank").href = "https://www.svenska.se/tre/?sok=" + ord + "&pz=1";
-			}
-		 }
+         if(this.status == 200) {
+            var nu = new Date().getTime();
+            var startDag = new Date("3 jun 2020").getTime();
+            var index = (Math.floor((nu / 1000 / 60 / 60 / 24) - (startDag / 1000 / 60 / 60 / 24)));
+            ordlista = JSON.parse(xhttp.responseText).ord;
+            while(index >= ordlista.length) {
+				   index -= ordlista.length;
+				   console.log(index);
+			   }
+			   ord = ordlista[index].namn.replace(/<script>/i, "VARNING");
+			   betydelse = ordlista[index].betydelse.replace(/<script>/i, "VARNING");
+		   	exempelmening = ordlista[index].exempelmening.replace(/<script>/i, "VARNING");
+			   etymologi = ordlista[index].etymologi.replace(/<script>/i, "VARNING");
+            
+            innehall("ord", ord.charAt(0).toUpperCase() + ord.substring(1));
+            innehall("rubrikbet", "Vad betyder <output class='kursiv'>" + ord + "</output>?");
+            innehall("rubrikanv", "Hur skulle man kunna använda ordet <output class='kursiv'>" + ord + "</output>?");
+            innehall("rubrikety", "Varifrån kommer ordet <output class='kursiv'>" + ord + "</output>?");
+            
+            innehall("betydelse", betydelse);
+            innehall("anvandning", exempelmening);
+            innehall("etymologi", etymologi);
+            
+            if(ordlista[index].lank !== undefined) {
+               document.getElementById("lank").href = ordlista[index].lank;
+            }
+            else {
+               document.getElementById("lank").href = "https://www.svenska.se/tre/?sok=" + ord + "&pz=1";
+            }
+            
+            if(ordlista[index].uttal !== undefined) {
+               document.getElementById("rubrikutt").style.display = "block";
+               document.getElementById("uttal").style.display = "block";
+               document.getElementById("rubrikutt").innerHTML = "Hur uttalar man <output class='kursiv'>" + ord + "</output>?";
+               document.getElementById("uttal").innerHTML = ordlista[index].uttal;
+            }
+            
+         }
          else  {
-			innehall("ord", "Fel");
-			innehall("rubrikbet", "Ajdå!");
-			innehall("betydelse", "Ett fel uppstod när orden skulle läsas in. Försök att läsa in sidan igen. Om felet kvarstår kan du behöva kontakta sidans ägare.");
-			innehall("rubrikety", "");
+            innehall("ord", "Fel");
+            innehall("rubrikbet", "Ajdå!");
+            innehall("betydelse", "Ett fel uppstod när orden skulle läsas in. Försök att läsa in sidan igen. Om felet kvarstår kan du behöva kontakta sidans ägare.");
+            innehall("rubrikety", "");
          
-			innehall("rubrikanv", "");
-			innehall("anvandning", "");
-			innehall("etymologi", "");
-		}
+            innehall("rubrikanv", "");
+            innehall("anvandning", "");
+            innehall("etymologi", "");
+            innehall("lank", "")
+         }
       }
-   }
+   };
    xhttp.open("GET", "https://raw.githubusercontent.com/dagensord/dagensord.github.io/master/ordlista.json", true);
    xhttp.send();
 }
